ef455631b8fe6d2fc3b7ace9a18fb6ae0c0f050f
jj <talk2jerry@gmail.com>
2024-03-08 23:30:14 +0000

    t

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-JavaScript
 function display(value) {
     document.getElementById("result").value += value;
 }
 
 function calculate() {
-    console.log();
     let p = document.getElementById("result").value;
     let q = eval(p);
     document.getElementById("result").value = q;
 }
 
 function clearScreen() {
-    console.log();
     document.getElementById("result").value = "";
-}
