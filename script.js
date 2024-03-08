473febe28af54fb5ebcaa009692386d21e002d88
jj <talk2jerry@gmail.com>
2024-03-08 23:24:17 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ function display(value) {
 }
 
 function calculate() {
+    console.log();
     let p = document.getElementById("result").value;
     let q = eval(p);
     document.getElementById("result").value = q;
 }
 
 function clearScreen() {
+    console.log();
     document.getElementById("result").value = "";
 }
