a2a1ff8093a167524534287951503f05e330661e
jj <talk2jerry@gmail.com>
2024-03-09 00:00:19 +0000

    w

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-function display(value) {
-    document.getElementById("result").value += value;
-}
-
-function calculate() {
-    let p = document.getElementById("result").value;
-    let q = eval(p);
-    document.getElementById("result").value = q;
-}
-
-function clearScreen() {
-    document.getElementById("result").value = "";
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function calculate() {
+    try {
+      document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+      document.getElementById('display').value = 'Error';
+    }
+  }
